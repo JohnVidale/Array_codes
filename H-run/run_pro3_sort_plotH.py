d634c7e000903faf74ebdc72aefa1316cf5f22f8
JohnVidale <seismoguy@mac.com>
2019-08-13 09:28:19 -0700

    added H-run directory

---
+++ H-run/run_pro3_sort_plotH.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# input is set of hinet traces
+# this program tapers, filters, selects range and SNR
+# plots against traveltime curves, either raw or reduced against traveltimes
+# This programs deals with a single event.
+# John Vidale 2/2019
+
+import os
+os.chdir('/Users/vidale/Documents/GitHub/Hinet-codes')
+from pro3a_sort_plot_pair import pro3pair
+from pro3b_sort_plot_singlet import pro3singlet
+
+os.environ['PATH'] += os.pathsep + '/usr/local/bin'
+
+os.chdir('/Users/vidale/Documents/PyCode/Hinet/Reps/Aleutians/Pair1')
+pro3singlet(eq_file = 'event1.txt', start_buff = 100,
+			end_buff = 300, plot_scale_fac = 0.1,qual_threshold = 3,
+			dphase = 'P', dphase2 = 'pP', dphase3 = 'PcP', dphase4 = 'sP',
+			corr_threshold = 0.7, freq_min = 1, freq_max = 3, min_dist = 25, max_dist = 44)
+
+#os.chdir('/Users/vidale/Documents/PyCode/Hinet/Reps/Aleutians/Pair1')
+#pro3pair(eq_file1 = 'event1.txt', eq_file2 = 'event2.txt', start_buff = 100,
+#			end_buff = 300, plot_scale_fac = 0.1,qual_threshold = 3,
+#			dphase = 'P', dphase2 = 'pP', dphase3 = 'PcP', dphase4 = 'sP',
+#			corr_threshold = 0.7, freq_min = 1, freq_max = 3, min_dist = 25, max_dist = 44)
